fa6c2a080198e6f5cfbc9aa0de296296b4558c92
Tom Smith <tom@solrr.net>
2018-09-17 23:23:49 -0600

    Say hi

---
+++ src/main/kotlin/com/skunk/demo/HiFoo.kt
@@ -0,0 +1,14 @@
+package com.skunk.demo
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RestController
+class HiFooController {
+
+    @RequestMapping("/")
+    fun index(): String {
+        return "Oh Kotlin is Hi-Foo"
+    }
+
+}
